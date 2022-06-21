f822b9e6b42c247b724acc31e9f5d58618ee6160
yuyt <yuyt@qaics.com>
2022-06-21 18:04:28 +0800

    init

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"embed"
 	"github.com/gin-gonic/gin"
+	"github.com/zserge/lorca"
 	"io/fs"
 	"net/http"
 	"os"
@@ -43,15 +44,66 @@ func main() {
 		router.Run(":9999")
 	}()
 
-	chromePath := "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:9999/static/index.html  --disable-translate --disable-sync")
-	cmd.Start()
-
+	chView := make(chan struct{})
+	chCMD := make(chan struct{})
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt)
+	go openView(chView, chCMD)
 
-	select {
-	case <-chSignal:
-		cmd.Process.Kill()
+	for {
+		select {
+		case <-chSignal:
+			chCMD <- struct{}{}
+		case <-chView:
+			os.Exit(0)
+		}
 	}
+
+}
+
+var defaultChromeArgs = []string{
+	"--disable-background-networking",
+	"--disable-background-timer-throttling",
+	"--disable-backgrounding-occluded-windows",
+	"--disable-breakpad",
+	"--disable-client-side-phishing-detection",
+	"--disable-default-apps",
+	"--disable-dev-shm-usage",
+	"--disable-infobars",
+	"--disable-extensions",
+	"--disable-features=site-per-process",
+	"--disable-hang-monitor",
+	"--disable-ipc-flooding-protection",
+	"--disable-popup-blocking",
+	"--disable-prompt-on-repost",
+	"--disable-renderer-backgrounding",
+	"--disable-sync",
+	"--disable-translate",
+	"--disable-windows10-custom-titlebar",
+	"--metrics-recording-only",
+	"--no-first-run",
+	"--no-default-browser-check",
+	"--safebrowsing-disable-auto-update",
+	"--password-store=basic",
+	"--use-mock-keychain",
+	"--app=http://127.0.0.1:9999/static/index.html",
+	"--disable-translate",
+	"--disable-sync",
+	"--remote-debugging-port=0",
+}
+
+func openView(chView chan struct{}, chCMD chan struct{}) {
+	//chromePath := "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+	chromePath := lorca.LocateChrome()
+	cmd := exec.Command(chromePath, defaultChromeArgs...)
+	cmd.Start()
+
+	go func() {
+		<-chCMD
+		cmd.Process.Kill()
+	}()
+	go func() {
+		cmd.Wait()
+		chView <- struct{}{}
+	}()
 }
